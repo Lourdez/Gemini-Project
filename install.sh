0386e32bed62d70e4f60c672e3eeb04dc4a64260
parker <54675124+Lourdez@users.noreply.github.com>
2025-05-01 01:08:53 +0530

    updated keys in install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,32 @@
+#!/bin/bash
 apt-get update -y
 
 apt install openjdk-21-jdk -y
 
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
   https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
-apt-get update -y
-apt-get install jenkins -y
+  /etc/apt/sources.list.d/jenkins.list >/dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
 systemctl start jenkins
 systemctl enable jenkins
 
-apt install docker.io -y
 
-apt install nodejs -y
+sudo apt-get install -y ca-certificates curl gnupg lsb-release apt-transport-https
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+  gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-apt install npm -y 
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo systemctl enable docker
+sudo systemctl start docker
 
 chmod 777 /var/run/docker.sock
